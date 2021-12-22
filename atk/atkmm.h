8b140429a8c5b2970bab57bb36303a1c72d31f2a
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2021-12-22 18:27:26 +0100

    atkmm.h: Atkmm is used by gtkmm-3.0, not by gtkmm-4.0

--- atk/atkmm.h
+++ atk/atkmm.h
@@ -20,12 +20,14 @@
 #ifndef _ATKMM_H
 #define _ATKMM_H
 
-
 /** @mainpage atkmm Reference Manual
  *
  * @section description Description
  *
- * atkmm is the official C++ interface for the <a href="http://library.gnome.org/devel/atk/stable/">ATK</a> accessibility toolkit library. It may be used, for instance, by user interfaces implemented with gtkmm.
+ * atkmm is the official C++ interface for the
+ * <a href="http://library.gnome.org/devel/atk/stable/">ATK</a>
+ * accessibility toolkit library. It may be used, for instance, by user interfaces
+ * implemented with gtkmm-3.0.
  *
  * @section basics Basic usage
  *
