c435189145bc206fadf43a258487dcd9ef4e6fb6
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2025-04-15 09:45:17 +0200

    MSVC_NMake/gendef/gendef.cc: License info: library -> program
    Was incorrectly corrected in the previous commit.

--- MSVC_NMake/gendef/gendef.cc
+++ MSVC_NMake/gendef/gendef.cc
@@ -13,7 +13,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this library; if not, see <https://www.gnu.org/licenses/>.
+ *  along with this program; if not, see <https://www.gnu.org/licenses/>.
  *
  *  For more information, visit the MICO Home Page at
  *  http://www.mico.org/
